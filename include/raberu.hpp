854a46962b56bb5327baa469d056f195ae49db14
Joel Falcou <joel.falcou@lri.fr>
2021-04-17 00:07:22 +0200

    Add a match::with() constexpr helper for validation of settings

--- include/raberu.hpp
+++ include/raberu.hpp
@@ -19,6 +19,10 @@ namespace rbr
     V value;
   };
 
+  template<typename... KWs> struct either_
+  {
+  };
+
   // Turn any type into a RegularType info carrier
   template<typename T> struct keyword_type
   {
@@ -27,6 +31,23 @@ namespace rbr
     template<typename V> constexpr auto operator|(V &&value) const noexcept
     {
       return type_or_<T, V> {RBR_FWD(value)};
+    }
+
+    template<typename U> constexpr auto operator|(keyword_type<U>) const noexcept
+    {
+      return either_<keyword_type<T>, keyword_type<U>> {};
+    }
+
+    template<typename... Kws>
+    friend constexpr auto operator|(either_<Kws...>, keyword_type) noexcept
+    {
+      return either_<Kws..., keyword_type<T>> {};
+    }
+
+    template<typename... Kws>
+    friend constexpr auto operator|(keyword_type, either_<Kws...>) noexcept
+    {
+      return either_<keyword_type<T>, Kws...> {};
     }
   };
 
@@ -48,37 +69,38 @@ namespace rbr
   namespace detail
   {
     // Turn a Type+Value pair into a Callable
-    template<typename Callable> struct linked_value : Callable
+    template<typename Key, typename Callable> struct linked_value : Callable
     {
       static constexpr bool is_parameter_type = true;
-      constexpr linked_value(Callable f) noexcept : Callable(f) {}
+      using key_type                          = Key;
+
+      constexpr linked_value(Key, Callable f) noexcept : Callable(f) {}
       using Callable::operator();
     };
+  }
 
+  // keyword parameter concept
+  template<typename T> concept keyword_parameter = std::remove_cvref_t<T>::is_parameter_type;
+
+  namespace detail
+  {
     // Build the type->value lambda capture
     template<typename Key, typename T> constexpr auto link(T &&v) noexcept
     {
-      if constexpr( std::is_lvalue_reference_v<T> )
-        return linked_value([&v](Key const &) constexpr->decltype(auto) { return v; });
+      if constexpr( keyword_parameter<std::decay_t<T>> )
+      {
+        return RBR_FWD(v);
+      }
+      else if constexpr( std::is_lvalue_reference_v<T> )
+      {
+        return linked_value(
+            Key {}, [&v](Key const &) constexpr->decltype(auto) { return v; });
+      }
       else
-        return linked_value([w = std::move(v)](Key const &) constexpr->T const & { return w; });
-    }
-
-    // Don't relink already linked values
-    template<typename Key, typename C> constexpr decltype(auto) link(linked_value<C> &v) noexcept
-    {
-      return v;
-    }
-
-    template<typename Key, typename C>
-    constexpr decltype(auto) link(linked_value<C> const &v) noexcept
-    {
-      return v;
-    }
-
-    template<typename Key, typename C> constexpr auto link(linked_value<C> &&v) noexcept
-    {
-      return std::move(v);
+      {
+        return linked_value(
+            Key {}, [w = std::move(v)](Key const &) constexpr->T const & { return w; });
+      }
     }
 
     // Type notifying that we can't find a given key
@@ -94,7 +116,7 @@ namespace rbr
     // Aggregate lambdas and give them a operator(Key)-like interface
     template<typename... Ts> struct aggregator : Ts...
     {
-      constexpr aggregator(Ts... t) noexcept : Ts(t)... {}
+      constexpr aggregator(Ts &&...t) noexcept : Ts(RBR_FWD(t))... {}
       using Ts::operator()...;
 
       template<typename K> constexpr auto operator()(keyword_type<K> const &) const noexcept
@@ -104,9 +126,6 @@ namespace rbr
       }
     };
   }
-
-  // keyword parameter concept
-  template<typename T> concept keyword_parameter = std::remove_cvref_t<T>::is_parameter_type;
 
   // Build a key-value from an option object
   template<typename T>
@@ -121,17 +140,37 @@ namespace rbr
   {
     using type = keyword_type<O>;
   };
+
+  template<typename O> struct tag<keyword_type<O>>
+  {
+    using type = keyword_type<O>;
+  };
+
+  template<typename K, typename C> struct tag<detail::linked_value<K, C>>
+  {
+    using type = K;
+  };
+
   template<typename O> using tag_t = typename tag<O>::type;
 
-  // settings is an unordered set of values accessible via their types
-  template<typename... Ts> struct settings
+  template<typename...> struct keys
   {
-    using parent = detail::aggregator<detail::linked_value<Ts>...>;
+  };
+  template<typename...> struct links
+  {
+  };
 
-    constexpr settings(detail::linked_value<Ts>... ts) : content_(ts...) {}
+  // settings is an unordered set of values accessible via their types
+  template<typename Keys, typename Links> struct settings;
+
+  template<typename... Ks, typename... Ts> struct settings<keys<Ks...>, links<Ts...>>
+  {
+    using keys_type  = keys<Ks...>;
+    using links_type = links<Ks...>;
+    using parent     = detail::aggregator<Ts...>;
 
     template<typename... Vs>
-    constexpr settings(Vs &&...v) : content_(detail::link<tag_t<std::decay_t<Vs>>>(RBR_FWD(v))...)
+    constexpr settings(Vs &&...v) : content_(detail::link<Ks>(RBR_FWD(v))...)
     {
     }
 
@@ -161,9 +200,32 @@ namespace rbr
         return tgt.value;
     }
 
+    // Pattern matcher
+    template<typename... KW> static inline constexpr bool validate(either_<KW...> const &) noexcept
+    {
+      auto is_ok = []<typename K>(K) { return (std::same_as<K, KW> || ... || false); };
+      return (is_ok(Ks {}) && ... && true);
+    }
+
+    template<typename K> static inline constexpr bool validate(keyword_type<K> const &) noexcept
+    {
+      return (std::same_as<Ks, keyword_type<K>> && ... && true);
+    }
+
     parent content_;
   };
 
   template<typename... Vs>
-  settings(Vs &&...v) -> settings<decltype(detail::link<tag_t<std::decay_t<Vs>>>(RBR_FWD(v)))...>;
+  settings(Vs &&...v)
+      -> settings<keys<tag_t<std::decay_t<Vs>>...>,
+                  links<decltype(detail::link<tag_t<std::decay_t<Vs>>>(RBR_FWD(v)))...>>;
+
+  // Pattern matcher entry point
+  template<typename... Args> struct match
+  {
+    template<typename Pattern> static inline constexpr bool with(Pattern p)
+    {
+      return decltype(rbr::settings {std::declval<Args>()...})::validate(p);
+    }
+  };
 }
